e3b6ca104d9ba190257db0f64907adac33ef6520
bencarneiro <bencarneiro@gmail.com>
2023-08-15 12:42:17 -0700

    add test endpoint

--- app/app/urls.py
+++ app/app/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from football.views import depth_chart
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("depth_chart/", depth_chart, name="depth_chart")
 ]
--- app/football/views.py
+++ app/football/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+
+from django.views import View
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+from football.models import DepthChart, PlayerPassing
+
+@csrf_exempt
+def depth_chart(request):
+    print(request.GET)
+    if "team" in request.GET and request.GET['team']:
+        chart = DepthChart.objects.filter(team_id__slug=request.GET['team'])
+        for ch in chart:
+            print(ch.player.name)
+            print(f"{ch.position.name} - {ch.string}")
+            pass_yds = PlayerPassing.objects.filter(player=ch.player, year=2022)
+            if len(pass_yds) > 0:
+                print(pass_yds[0].pass_yds)
+        return JsonResponse({"Hi": "Hello"})
+
+    return JsonResponse({"Good": "Bye"})
