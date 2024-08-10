e683ad73d0ed9af847319420996219f1bcb49bf2
bencarneiro <bencarneiro@gmail.com>
2024-08-09 20:57:45 -0500

    add a bash script to run the python import scripts

---
+++ app/import_game_data.sh
@@ -0,0 +1,4 @@
+python /home/tonydeals/app/fantasy/app/manage.py read_game_stats_2020
+python /home/tonydeals/app/fantasy/app/manage.py read_game_stats_2021
+python /home/tonydeals/app/fantasy/app/manage.py read_game_stats_2022
+python /home/tonydeals/app/fantasy/app/manage.py read_game_stats_2023
